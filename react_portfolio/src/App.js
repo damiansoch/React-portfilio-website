ccc11137e1cc89734f87707b2828c6923d3bf595
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-06-19 08:40:51 +0100

    Styled About

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
 import AboutUs from "./pages/AboutUs";
+// global style
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
-  return <AboutUs />;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <AboutUs />
+    </div>
+  );
 }
 
 export default App;
